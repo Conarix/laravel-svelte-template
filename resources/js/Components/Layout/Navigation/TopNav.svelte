e1aeb297e695b19f8feb5fde19f03257dd63b6ce
Connor-Lock05 <monkeynutz1708@gmail.com>
2025-09-14 16:33:32 +0100

    style: reduce shadow on top header

--- resources/js/Components/Layout/Navigation/TopNav.svelte
+++ resources/js/Components/Layout/Navigation/TopNav.svelte
@@ -11,7 +11,7 @@
 
 </script>
 
-<header class="relative flex justify-between items-center w-full gap-4 px-4 py-2 bg-sidebar border-b shadow-lg">
+<header class="relative flex justify-between items-center w-full gap-4 px-4 py-2 bg-sidebar border-b shadow-md">
     <div class="flex justify-start items-center gap-8">
         <div class="flex justify-center items-center w-full">
             <AppLogo/>
